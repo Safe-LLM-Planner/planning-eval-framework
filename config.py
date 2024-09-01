92c7e9fb8e9fd0d9aaf91de9e46e6c27cb46d9b1
Agustín Martinez Suñé <4691852+aemartinez@users.noreply.github.com>
2024-09-01 22:14:04 +0100

    Reviewed experiments

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ DEFAULT_PYD_GENERATORS = {
     "llm_stepbystep": "strict_actions"
 }
 DEFAULT_PLAN_MATCHER = "greedy_action"
+OPENAI_MODEL = "gpt-4o-2024-08-06"
+# OPENAI_MODEL = "gpt-4o-mini-2024-07-18"
